978d3f6e4b8d6537e5cd30511f392eab083db0a0
HeliaMazinani <13helia1383@gmail.com>
2024-12-16 14:35:01 +0330

    kk

---
+++ GUItkinter.py
@@ -0,0 +1,29 @@
+from tkinter import *
+
+def on_login():
+    Myusername = e1.get()
+    Mypassword = e2.get()
+    print(f"Username: {Myusername}, Password: {Mypassword}")
+
+top = Tk()hhbh
+top.geometry("400x300")
+
+# Creating Labels
+username = Label(top, text="Username")
+username.place(x=60, y=50)
+
+password = Label(top, text="Password")
+password.place(x=60, y=90)
+
+# Creating Entry widgets
+e1 = Entry(top)
+e1.place(x=160, y=50)
+
+e2 = Entry(top, show="*")  # Adding show="*" to hide password input
+e2.place(x=160, y=90)
+
+# Creating the login button
+submit = Button(top, text='Login', command=on_login)
+submit.place(x=75, y=130)
+
+top.mainloop()
